42c4299527ad6311fe0113a3c9e6adc84b4e08cf
kian kheirani <kiankheirani@gmail.com>
2024-07-30 17:01:52 +0330

    '18'

--- app/card/page.tsx
+++ app/card/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Card from "@/components/Card";
+import Cart from "@/components/Card";
 export default function page() {
   return (
-    <div>
-      <Card></Card>
-    </div>
+    <>
+      <Cart></Cart>
+    </>
   );
 }
