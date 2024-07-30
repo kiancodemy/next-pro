bca5e4778b3f40248a06565f317e969e1ebf8956
kian kheirani <kiankheirani@gmail.com>
2024-07-30 16:34:46 +0330

    'debug14'

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fa">
       <body
         className={`${iran.variable}  flex flex-col justify-between font-iran dark:bg-night`}
       >
