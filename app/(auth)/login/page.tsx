82aa4f1796e61a8e58b78e55d8119b7a6e4b9a9f
kian <kiankheirani@gmail.com>
2025-03-04 11:23:39 +0330

    'fix-8'

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Login } from "@/components/Login";
+import MainLogin from "@/components/MainLogin";
 export default function page() {
   return (
     <div className="md:py-20 py-10">
-      <Login></Login>
+      <MainLogin></MainLogin>
     </div>
   );
 }
