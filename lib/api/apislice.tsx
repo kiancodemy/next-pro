a460dc5bb2dc3d8e01d94d442b28eac5cdabcf47
kian <kiankheirani@gmail.com>
2025-03-04 12:09:55 +0330

    'fix-8'

--- lib/api/apislice.tsx
+++ lib/api/apislice.tsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.NEXT_PUBLIC_BACKEND_URL}`,
+    baseUrl: `${process.env.NEXT_PUBLIC_BACKEND_URL as string}`,
   }),
   tagTypes: ["Getbyid"],
 
